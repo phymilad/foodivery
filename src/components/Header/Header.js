022eb3e61819cc19128a8166794981c08d62d89f
Milad Mohammadi <phy.milad@gmail.com>
2022-09-23 20:10:28 +0330

    initial style of header is done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,13 @@
 import React from "react"
-
+import "../../styles/header.scss"
+import logo from "../../assets/images/res-logo.png"
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div>others</div>
+    </div>
+  )
 }
