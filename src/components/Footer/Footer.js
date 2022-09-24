a7e2ae97de5de9e87472ca73d2ff3f925f92ab48
Milad Mohammadi <phy.milad@gmail.com>
2022-09-24 19:16:04 +0330

    mobile footer is complited

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,46 @@
 import React from "react"
 import "../../styles/Footer.scss"
+import logo from "../../assets/images/res-logo.png"
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUser } from "@fortawesome/free-solid-svg-icons"
 
 export default function Footer() {
   return (
     <div className="footer__container">
-      <div>dasjkf</div>
-      <div>asdk;fjlak</div>
-      <div>aslkjflkasf</div>
+      <div className="footer__logo">
+        <img src={logo} alt="Foodivery logo" />
+        <h5>Foodivery</h5>
+      </div>
+      <div className="footer__schedule">
+        <h3>Delivery Time</h3>
+        <h5>Sunday - Thursday</h5>
+        <p>11:00 am - 11:00 pm</p>
+        <h5>Friday - Saturday</h5>
+        <p>Off days</p>
+      </div>
+      <div className="contact">
+        <h3>Contact</h3>
+        <h5>Phone: 09357064101</h5>
+        <h5>Email: phy.milad@gmail.com</h5>
+      </div>
+      <div className="footer__social">
+        <h3>Follow us</h3>
+        <span className="user">
+          <Link to="/">
+            <FontAwesomeIcon icon={faUser} size="1.5x" />
+          </Link>
+          <Link to="/">
+            <FontAwesomeIcon icon={faUser} size="1.5x" />
+          </Link>
+          <Link to="/">
+            <FontAwesomeIcon icon={faUser} size="1.5x" />
+          </Link>
+          <Link to="/">
+            <FontAwesomeIcon icon={faUser} size="1.5x" />
+          </Link>
+        </span>
+      </div>
     </div>
   )
 }
