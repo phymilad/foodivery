806db9760601b56ff47bb16f1dd9a73ea5c09bc5
Milad Mohammadi <phy.milad@gmail.com>
2022-09-28 18:21:54 +0330

    tablet design of home page finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,21 +20,21 @@ const featureData = [
     title: "Quick Delivery",
     imgUrl: featureImage01,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
     minus: "doloremque1",
   },
   {
     title: "Quick Delivery",
     imgUrl: featureImage02,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
     minus: "doloremque2",
   },
   {
     title: "Quick Delivery",
     imgUrl: featureImage03,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.",
     minus: "doloremque3",
   },
 ]
@@ -42,35 +42,38 @@ const featureData = [
 export default function Home() {
   return (
     <div className="home__container">
-      <section className="home__description-container">
-        <h5>Easy way to make an order</h5>
-        <h2>
-          <span>HUNGRY?</span>Just Wait
-        </h2>
-        <h3>
-          Food at <span>your door</span>
-        </h3>
-        <p>This resturaunt takes your online orders</p>
-        <p>and deliver them to you asap</p>
-      </section>
+      <div className="home__introduction-section">
+        <div className="home__introduction-right">
+          <section className="home__description-container">
+            <h5>Easy way to make an order</h5>
+            <h2>
+              <span>HUNGRY?</span>Just Wait
+            </h2>
+            <h3>
+              Food at <span>your door</span>
+            </h3>
+            <p>This resturaunt takes your online orders</p>
+            <p>and deliver them to you asap</p>
+          </section>
 
-      <section className="order__link-container">
-        <Link className="order__link-order">Order Now</Link>
-        <Link to={"/foods"} className="order__link-foods">
-          See all foods
-        </Link>
-      </section>
+          <section className="order__link-container">
+            <Link className="order__link-order">Order Now</Link>
+            <Link to={"/foods"} className="order__link-foods">
+              See all foods
+            </Link>
+          </section>
 
-      <section className="delivery__info">
-        <p>No shipping charge</p>
-        <p>100% Secure Checkout</p>
-      </section>
-      <section>
-        <div className="delivery__image-container">
-          <img src={deliveryImg} alt="delivery bike" />
+          <section className="delivery__info">
+            <p>No shipping charge</p>
+            <p>100% Secure Checkout</p>
+          </section>
         </div>
-      </section>
-
+        <div className="home__introduction-left">
+          <div className="delivery__image-container">
+            <img src={deliveryImg} alt="delivery bike" />
+          </div>
+        </div>
+      </div>
       <Categories />
       <section className="home__message-to-customers">
         <h5>
@@ -80,8 +83,14 @@ export default function Home() {
         <h2>
           We will <strong>take care</strong>
         </h2>
-        <p>lorem ipsum is a testimonial text here</p>
-        <p>lorem ipsum is a testimonial text</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod
+        </p>
       </section>
       <section className="home__features-container">
         {featureData.map((feature, index) => {
@@ -125,36 +134,30 @@ export default function Home() {
           })}
         </div>
       </section>
+
       <section className="location__container">
         <img src={location} alt="location" />
-        <h2>
-          Why <span>Foodivery?</span>
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua
-        </p>
-        <div className="location__options-container">
-          <div className="location__option">
-            <h4>Fresh and tasty food</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt
-            </p>
-          </div>
-          <div className="location__option">
-            <h4>Quality support</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt
-            </p>
-          </div>
-          <div className="location__option">
-            <h4>Order from any location</h4>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt
-            </p>
+        <div>
+          <h2>
+            Why <span>Foodivery?</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </p>
+          <div className="location__options-container">
+            <div className="location__option">
+              <h4>Fresh and tasty food</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            </div>
+            <div className="location__option">
+              <h4>Quality support</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            </div>
+            <div className="location__option">
+              <h4>Order from any location</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            </div>
           </div>
         </div>
       </section>
