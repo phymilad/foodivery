dabb1a12c9484623a8c4e80a028f9302b77416d3
Milad Mohammadi <phy.milad@gmail.com>
2022-11-05 12:33:31 +0330

    compelete mobile style of food page

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -6,10 +6,15 @@ import products from "../assets/fake-data/products"
 export default function Food() {
   const params = useParams()
   const [product, setProduct] = useState({})
+  const [selectedImage, setSelectedImage] = useState("image01")
 
   useEffect(() => {
     setProduct(products.find((item) => item.id === params.id))
   }, [])
+
+  const handleSelectImage = (imageName) => {
+    setSelectedImage(imageName)
+  }
 
   console.log(product)
 
@@ -19,16 +24,33 @@ export default function Food() {
       <div className="food-image-info__container">
         <div className="food-images__container">
           <div className="food-thumbnails__container">
-            <img src={product.image01} alt="image1" />
-            <img src={product.image02} alt="image2" />
-            <img src={product.image03} alt="image3" />
+            <img
+              src={product.image01}
+              alt="image1"
+              onClick={() => handleSelectImage("image01")}
+            />
+            <img
+              src={product.image02}
+              alt="image2"
+              onClick={() => handleSelectImage("image02")}
+            />
+            <img
+              src={product.image03}
+              alt="image3"
+              onClick={() => handleSelectImage("image03")}
+            />
           </div>
-          <img src={product.image01} alt="main image" />
+          <img src={product[selectedImage]} alt="main image" />
         </div>
-        <div className="food-info__container">Info</div>
+        <div className="food-info__container">
+          <h3>{product.title}</h3>
+          <h4>category: {product.category}</h4>
+          <h5>price: {product.price}$</h5>
+          <button className="button_link ">Add to cart</button>
+        </div>
       </div>
       <div className="food-extra-info__container">
-        Extra information container
+        <p>{product.desc}</p>
       </div>
     </div>
   )
